fc565dd1570356866f7e801240dc3d15bf464c30
Peter King <techking61@outlook.com>
2019-07-31 21:10:04 -0400

    add dynamic templates

--- index.js
+++ index.js
@@ -14,12 +14,36 @@ const server = http.createServer((req, res) => {
 
     if(pathName === "/products" || pathName === "/"){
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`this is the ${pathName} page for laptop ${id}`);
+        
+        //we are rendering the basic HTML without dynamic content first
+        fs.readFile(`${__dirname}/templates/template-overview.html`, `utf-8`, (err, data) => {
+            //we are looping through to put an undertermined amount of laptops on display
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`, `utf-8`, (err, data) => {
+                //creating an array of all the HTML strings for each laptop && joining them into one long string
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join("");
+                //taking in initial HTML && replacing placeholder with the long string
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                
+                res.end(overviewOutput);
+            }); 
+
+        }); 
+
     }
 
-    else if (pathName === '/laptop' && (id < laptopData.length || id === undefined)){
+    else if (pathName === '/laptop' && id < laptopData.length){
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`this is the ${pathName} page`);
+        
+
+        // this is like what template engines do
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, `utf-8`, (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        }); 
+
+
     }
 
     //notice the use of 404
@@ -39,3 +63,17 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('listening for requests');
 });
+
+function replaceTemplate(origHTML, laptop) {
+
+    let output = origHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
